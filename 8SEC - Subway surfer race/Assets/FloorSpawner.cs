47140dd47f45af85d7a90c412ca5bf80ce224d36
Tcartegnie <Sorakssis@gmail.com>
2020-09-29 18:58:09 +0200

    Refacto the floor spawner

--- 8SEC - Subway surfer race/Assets/FloorSpawner.cs
+++ 8SEC - Subway surfer race/Assets/FloorSpawner.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class FloorSpawner : MonoBehaviour
 {
-	public GameObject BasicRoadPattern;
+	public GameObject EmptyRoadPattern;
 	public Transform RoadSpawnPoint;
 	public RoadPatternList roadLists;
 	public int SpawnRoadNumber;
-	public bool SpanwCamionEnable;
+	public bool SpawnCamionEnable;
 	RoadManager roadManager;
 
 
-	List<GameObject> TransformRoad = new List<GameObject>();
+	public List<GameObject> TransformRoad = new List<GameObject>();
 
 	GameObject LastRoad;
 	public void Awake()
@@ -24,7 +24,7 @@ public class FloorSpawner : MonoBehaviour
 	{
 		for (int j = 0; j < SpawnRoadNumber; j++)
 		{
-			GameObject GO = Instantiate(BasicRoadPattern, RoadSpawnPoint.position + ((Vector3.forward * 39) * j), new Quaternion());
+			GameObject GO = Instantiate(EmptyRoadPattern, RoadSpawnPoint.position + ((Vector3.forward * 20) * j), new Quaternion());
 			TransformRoad.Add(GO);
 			LastRoad = GO;
 		}
@@ -35,7 +35,7 @@ public class FloorSpawner : MonoBehaviour
 	{
 		for (int i = 0; i < TransformRoad.Count; i++)
 		{
-			TransformRoad[i].GetComponent<Road>().ChangeRoadPattern(BasicRoadPattern);
+			TransformRoad[i].GetComponent<Road>().ChangeRoadPattern(EmptyRoadPattern);
 		}
 	}
 
@@ -43,16 +43,16 @@ public class FloorSpawner : MonoBehaviour
 	public void resetRoadSegement(int segementID)
 	{
 		Road CurrentRoad = TransformRoad[segementID].GetComponent<Road>();
-		TransformRoad[segementID].transform.position = LastRoad.transform.position + transform.forward * 39;
-		CurrentRoad.ChangeRoadPattern(roadManager.GetCurrentSegement());
+		CurrentRoad.ChangeRoadPattern(roadManager.GetNextSegement());
+		TransformRoad[segementID].transform.position = LastRoad.transform.position + transform.forward * 20;
 		LastRoad = TransformRoad[segementID];
 	}
 
-	public void FixedUpdate()
+	public void Update()
 	{
 		for (int  i = 0;i < TransformRoad.Count;i++)
 		{
-			if(TransformRoad[i].transform.position.z < -39)
+			if(TransformRoad[i].transform.position.z < -20)
 			{
 				resetRoadSegement(i);
 			}
