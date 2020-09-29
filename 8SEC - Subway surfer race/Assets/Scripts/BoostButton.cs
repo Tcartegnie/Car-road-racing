06ab02f1c97e7abada67c7d16f2558f33753d9c5
Tcartegnie <Sorakssis@gmail.com>
2020-09-29 19:05:43 +0200

    Refacto boost button

--- 8SEC - Subway surfer race/Assets/Scripts/BoostButton.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/BoostButton.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class BoostButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
