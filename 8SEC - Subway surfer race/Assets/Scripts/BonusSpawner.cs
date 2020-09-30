9534b5fd67b08aa4e236da08c0e494e1c5578685
Tcartegnie <Sorakssis@gmail.com>
2020-09-30 18:15:40 +0200

    Add a script "Bonus spawner"

---
+++ 8SEC - Subway surfer race/Assets/Scripts/BonusSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawner : MonoBehaviour
+{
+	List<GameObject> CurrentObjects = new List<GameObject>();
+	public List<Transform> BonusSpawn = new List<Transform>();
+	public BonusList bonusList;
+	public void spawnbonus()
+	{
+		for (int i = 0; i < BonusSpawn.Count; i++)
+		{
+			BonusData data = bonusList.GetRandomBonus();
+			if (Random.Range(0, data.RandomRate) == 1)
+			{
+				GameObject go = ObjectPooler.instance.SpawnFromPool("bonus" + data.Name, new Vector3(BonusSpawn[i].position.x, BonusSpawn[i].position.y, BonusSpawn[i].position.z), new Quaternion());
+				go.transform.SetParent(transform, true);
+				CurrentObjects.Add(go);
+			}
+		}
+	}
+
+
+	public void RemoveBonus()
+	{
+		for (int i = 0; i < CurrentObjects.Count; i++)
+		{
+			CurrentObjects[i].SetActive(false);
+			CurrentObjects[i].transform.SetParent(null);
+		}
+		CurrentObjects.Clear();
+	}
+}
