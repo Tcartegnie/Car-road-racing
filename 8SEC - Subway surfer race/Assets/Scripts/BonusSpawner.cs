9ebc2200a17cfa0570d72e8dc49599e74d84255a
Tcartegnie <Sorakssis@gmail.com>
2020-10-01 23:33:15 +0200

    Refacto bonus spawner

--- 8SEC - Subway surfer race/Assets/Scripts/BonusSpawner.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/BonusSpawner.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class BonusSpawner : MonoBehaviour
 {
+
+	public void Start()
+	{
+		SpawnBonus();
+	}
+
 	List<GameObject> CurrentObjects = new List<GameObject>();
 	public List<Transform> BonusSpawn = new List<Transform>();
 	public BonusList bonusList;
-	public void spawnbonus()
+	public void SpawnBonus()
 	{
 		for (int i = 0; i < BonusSpawn.Count; i++)
 		{
 			BonusData data = bonusList.GetRandomBonus();
 			if (Random.Range(0, data.RandomRate) == 1)
 			{
-				GameObject go = ObjectPooler.instance.SpawnFromPool("bonus" + data.Name, new Vector3(BonusSpawn[i].position.x, BonusSpawn[i].position.y, BonusSpawn[i].position.z), new Quaternion());
+				GameObject go = ObjectPooler.instance.SpawnFromPool("Bonus" + data.Name, new Vector3(BonusSpawn[i].position.x, BonusSpawn[i].position.y, BonusSpawn[i].position.z), new Quaternion());
 				go.transform.SetParent(transform, true);
 				CurrentObjects.Add(go);
 			}
