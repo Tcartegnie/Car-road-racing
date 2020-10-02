02fc962db7309d602567d8000b84c9f73446690e
Tcartegnie <Sorakssis@gmail.com>
2020-10-02 18:47:46 +0200

    Add sound on  coins

--- 8SEC - Subway surfer race/Assets/Scripts/Coin.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/Coin.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Coin : CollectibleObject
 {
+	public AudioClip test;
+	public AudioSource CoinNoise;
 	public int Value;
 	public override void UseBonus(GameObject other)
 	{
 		other.GetComponentInParent<Score>().AddScore(Value);
+		CoinNoise.PlayOneShot(test);
 	}
 }
