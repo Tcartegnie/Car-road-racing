07591c57fd6c93f8a11eb9cb0ea3f7284176fe5e
Tcartegnie <Sorakssis@gmail.com>
2020-10-30 18:22:46 +0100

    refacto the car movement transition WIP

--- 8SEC - Subway surfer race/Assets/Scripts/CarCinematiqueMovement.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/CarCinematiqueMovement.cs
@@ -7,6 +7,7 @@ public class CarCinematiqueMovement : MonoBehaviour
     public float TransitionDuration;
 	public Transform Start;
 	public Transform Car;
+	public Transform EndPosition;
 	public AnimationCurve Transition;
    public void CallTransition()
 	{
@@ -16,12 +17,11 @@ public class CarCinematiqueMovement : MonoBehaviour
     IEnumerator CarTransition()
 	{
 		Car.position = Start.position;
-		Vector3 FinalPosition = new Vector3(0,1,0);
+		Vector3 FinalPosition = EndPosition.position;
         for(float i = 0;i < 1; i+= Time.deltaTime / TransitionDuration)
 		{
-			Car.position = Vector3.Lerp(Start.position, FinalPosition, Transition.Evaluate(i));
+			Car.position = Vector3.Lerp(Start.position, FinalPosition, i);
 			yield return null;
-
 		}
 		Car.position = Vector3.Lerp(Start.position, FinalPosition, 1);
 	}
