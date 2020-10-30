0b011bc0aba89755efcd908eeba068166cfcf003
Tcartegnie <Sorakssis@gmail.com>
2020-10-30 18:23:06 +0100

    Refacto the whole code to fit with the new hierachy

--- 8SEC - Subway surfer race/Assets/Scripts/BonusSpawner.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/BonusSpawner.cs
@@ -2,39 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BonusSpawner : MonoBehaviour
+public class BonusSpawner : ObjectGenerator
 {
 
 	public void Start()
 	{
 		SpawnBonus();
 	}
-
-	List<GameObject> CurrentObjects = new List<GameObject>();
-	public List<Transform> BonusSpawn = new List<Transform>();
+	
 	public BonusList bonusList;
 	public void SpawnBonus()
 	{
-		for (int i = 0; i < BonusSpawn.Count; i++)
+		for (int i = 0; i < Spawns.Count; i++)
 		{
 			BonusData data = bonusList.GetRandomBonus();
 			if (Random.Range(0, data.RandomRate) == 1)
 			{
-				GameObject go = ObjectPooler.instance.SpawnFromPool("Bonus" + data.Name, new Vector3(BonusSpawn[i].position.x, BonusSpawn[i].position.y, BonusSpawn[i].position.z), new Quaternion());
-				go.transform.SetParent(transform, true);
-				CurrentObjects.Add(go);
+				GenerateObject("Bonus" + data.Name,i, Spawns[i].position);
 			}
 		}
 	}
 
 
-	public void RemoveBonus()
-	{
-		for (int i = 0; i < CurrentObjects.Count; i++)
-		{
-			CurrentObjects[i].SetActive(false);
-			CurrentObjects[i].transform.SetParent(null);
-		}
-		CurrentObjects.Clear();
-	}
+	
 }
