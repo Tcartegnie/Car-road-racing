c924714f934f2527e574eddbbda8f724918786ee
Tcartegnie <Sorakssis@gmail.com>
2020-11-23 17:59:37 +0100

    Update KillCollider.cs

--- 8SEC - Subway surfer race/Assets/Scripts/KillCollider.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/KillCollider.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class KillCollider : MonoBehaviour
 {
+	public Score score;
+	public int ScorePerBounce;
+	public CarStat carstate;
+	bool Invulnerable;
+
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.tag == "Player")
-		{
-
-			other.GetComponentInParent<CarStat>().CallKillCar();
-			//other.gameObject.SetActive(false);
-		}
+		CallCollision(other);
 	}
 
+	void SetInvulnerability(bool value)
+	{
+		Invulnerable = value;
+	}
+	public void TurnInvulnerabilityOn()
+	{
+		SetInvulnerability(true);
+	}
+	public void TurnInvulnerabilityOff()
+	{
+		SetInvulnerability(false);
+	}
+
+	public void CallCollision(Collider other)
+	{
+		if (Invulnerable)
+		{
+			other.GetComponentInChildren<BounceScript>().Bounce();
+			score.AddScore(ScorePerBounce);
+		}
+		else
+		{
+			carstate.CallKillCar();
+		}
+	}
 }
