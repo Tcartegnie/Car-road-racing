bdbc86390c13d239805d012a5affeabc6776e952
Tcartegnie <Sorakssis@gmail.com>
2020-11-23 17:59:53 +0100

    Update CoinGenerator.cs

--- 8SEC - Subway surfer race/Assets/Scripts/ObjectGenerator/CoinGenerator.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/ObjectGenerator/CoinGenerator.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class CoinGenerator : ObjectGenerator
 {
+	private Score score;
 	public int CoinNumber;
 	public float SpaceBetweenCoin;
 
-	public void GenerateCoins()
+	public Score Score { get => score; set => score = value; }
+
+	public void GenerateCoins(RoadSegements segement)
 	{
-		for (int i = 0; i < Spawns.Count; i++)
+		for (int i = 0; i < segement.Train.Length; i++)
 		{
-			//int RandomResult = Random.Range(0, 2);
-			//if (RandomResult == 1)
-			//{
-				for (int j = 0; j < CoinNumber; j++)
+			if (segement.IsCoinSpawner[i])
+			{
+				if (Random.Range(0, 2) == 1)
 				{
-					GenerateObject("Coin",i, Spawns[i].forward * -(SpaceBetweenCoin * j));
+					if (segement.Train[i])//Check if there is on train on this lane
+					{//If true we set the coins position over the train
+						for (int j = 0; j < CoinNumber; j++)
+						{
+							CreateCoin(i, (Spawns[i].up * 7) + (Spawns[i].forward * -(SpaceBetweenCoin * j)));
+						}
+					}
+					else//If there is no train
+					{//We set the coin, at the normal position
+						for (int j = 0; j < CoinNumber; j++)
+						{
+							CreateCoin(i, Spawns[i].forward * -(SpaceBetweenCoin * j));
+						}
+					}
 				}
-			//}
+			}
 		}
+	}
+	public void CreateCoin(int SpawnID,Vector3 offset)
+	{
+		GameObject GO = GenerateObject("Coin", SpawnID, (offset));
+		GO.GetComponent<Coin>().Score = Score;
 	}
 
 }
