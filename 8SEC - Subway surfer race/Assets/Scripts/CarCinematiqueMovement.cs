05763d064b8af280725a0b6a278b460d5c37d684
Tcartegnie <Sorakssis@gmail.com>
2020-11-23 17:56:41 +0100

    Refacto CarCInematique systéme
    Remove useless corroutine fix the bug with the car who dont transit on the road

--- 8SEC - Subway surfer race/Assets/Scripts/CarCinematiqueMovement.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/CarCinematiqueMovement.cs
@@ -7,22 +7,51 @@ public class CarCinematiqueMovement : MonoBehaviour
     public float TransitionDuration;
 	public Transform Start;
 	public Transform Car;
-	public Transform EndPosition;
+	public Transform EndPositionIn;
+	public Transform StartPositionOut;
+	public Transform EndPositionOut;
 	public AnimationCurve Transition;
-   public void CallTransition()
+	
+   public void CallTransitionIn()
 	{
-		StartCoroutine(CarTransition());
+		StartCoroutine(CarTransitionIn());
 	}
 
-    IEnumerator CarTransition()
+	public void CallTransitionOut()
 	{
-		Car.position = Start.position;
-		Vector3 FinalPosition = EndPosition.position;
+		StartCoroutine(CarTransitionOut());
+	}
+
+	public void CallCompleteTransition()
+	{
+		StartCoroutine(CompleteTransition());
+	}
+
+	IEnumerator CompleteTransition()
+	{
+		yield return StartCoroutine(CarTransition(Start, StartPositionOut));
+		yield return StartCoroutine(CarTransition(StartPositionOut,EndPositionOut));
+	}
+
+    IEnumerator CarTransition(Transform StartPosition,Transform ReachPosition)
+	{
+		Car.position = StartPosition.position;
+		Vector3 start = StartPosition.position;
+		Vector3 destination = ReachPosition.position;
         for(float i = 0;i < 1; i+= Time.deltaTime / TransitionDuration)
 		{
-			Car.position = Vector3.Lerp(Start.position, FinalPosition, i);
+			Car.position = Vector3.Lerp(start, destination, i);
 			yield return null;
 		}
-		Car.position = Vector3.Lerp(Start.position, FinalPosition, 1);
+		Car.position = Vector3.Lerp(start, destination, 1);
+	}
+
+	public IEnumerator CarTransitionIn()
+	{
+		yield return StartCoroutine(CarTransition(Start, EndPositionIn));
+	}
+	public IEnumerator CarTransitionOut()
+	{
+		yield return StartCoroutine(CarTransition(StartPositionOut, EndPositionOut));
 	}
 }
