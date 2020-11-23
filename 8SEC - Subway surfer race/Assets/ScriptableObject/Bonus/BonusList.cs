77a031d51350aaca08aa3b1acc2ce874418d0c78
Tcartegnie <Sorakssis@gmail.com>
2020-11-23 18:03:12 +0100

    Add new data in bonus list

--- 8SEC - Subway surfer race/Assets/ScriptableObject/Bonus/BonusList.cs
+++ 8SEC - Subway surfer race/Assets/ScriptableObject/Bonus/BonusList.cs
@@ -7,12 +7,22 @@ public struct BonusData
 {
 	public string Name;
 	public int RandomRate;
+	public int ID;
 }
 
 [CreateAssetMenu(fileName = "Bonus", menuName = "ScriptableObjects/BonusList", order = 1)]
 public class BonusList : ScriptableObject
 {
 	public List<BonusData> Bonus;
+
+	public BonusData GetBonusByID(int ID)
+	{
+		return Bonus.Find(BonusName => BonusName.ID == ID);
+	}
+	public BonusData GetBonusByName(string name)
+	{
+		return Bonus.Find(BonusName => BonusName.Name == name);
+	}
 
 	public BonusData GetRandomBonus()
 	{
