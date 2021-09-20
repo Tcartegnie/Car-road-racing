966d9468b5068fa359bb39b4a856ffbe8b20c006
Tcartegnie <Sorakssis@gmail.com>
2021-09-20 20:38:16 +0200

    Update EndGame.cs

--- 8SEC - Subway surfer race/Assets/Scripts/Gameplay/EndGame/EndGame.cs
+++ 8SEC - Subway surfer race/Assets/Scripts/Gameplay/EndGame/EndGame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EndGame : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class EndGame : MonoBehaviour
 	public InGameUI IGUI;
 
 	public float CoolDownAfterCount;
+
 
 	public void PlayDisplayScore()
 	{
