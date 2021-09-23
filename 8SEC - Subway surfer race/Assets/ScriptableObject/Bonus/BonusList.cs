91146681fbfde550d5db09a6f0eaf2f9d4525a3b
Tcartegnie <Sorakssis@gmail.com>
2021-09-23 17:41:48 +0200

    Refacto bonus list

--- 8SEC - Subway surfer race/Assets/ScriptableObject/Bonus/BonusList.cs
+++ 8SEC - Subway surfer race/Assets/ScriptableObject/Bonus/BonusList.cs
@@ -9,18 +9,13 @@ public struct BonusData
 	public string Name;
 	public float Duration;
 	public int RandomRate;
-	public int ID;
 }
+
 
 [CreateAssetMenu(fileName = "Bonus", menuName = "ScriptableObjects/BonusList", order = 1)]
 public class BonusList : ScriptableObject
 {
 	public List<BonusData> Bonus;
-
-	public BonusData GetBonusByID(int ID)
-	{
-		return Bonus.Find(BonusName => BonusName.ID == ID);
-	}
 	public BonusData GetBonusByName(string name)
 	{
 		return Bonus.Find(BonusName => BonusName.Name == name);
